e4dbc5cffc29246b20064208ff05b98e052e8c1f
Ankush Ganesh <ankush.ganesh@promobitech.com>
2025-07-14 15:36:37 +0530

    Tutorial - SwiftUIBasics

---
+++ SwiftUIBasics/SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Ankush Ganesh on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
